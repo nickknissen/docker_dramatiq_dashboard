dc76db6c8277f2a9a9e478b5390f0ceff6139e33
Nick Nissen <nick.nissen@uptimedevelopment.dk>
2019-08-27 15:06:50 +0200

    declare queues from envrioment variables

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,4 @@ COPY ["requirements.txt", "run.py", "/app/"]
 
 RUN python3 -m pip install -r requirements.txt --no-cache-dir
 
-
 CMD ["gunicorn", "run:app", "--bind 0.0.0.0:8000"]
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+
 import dramatiq
 
 from dramatiq_dashboard import DashboardApp
@@ -6,6 +7,11 @@ from dramatiq.brokers.redis import RedisBroker
 
 REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379")
 
+QUEUES = os.getenv("QUEUES", "default,")
+
 broker = RedisBroker(url=REDIS_URL)
+
+[broker.declare_queue(queue) for queue in list(filter(None, QUEUES.split(",")))]
+
 dramatiq.set_broker(broker)
 app = DashboardApp(broker=broker, prefix="")
